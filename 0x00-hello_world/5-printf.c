0f5de0560a073506d4bb4983fdbf3d4b9504722a
morufat-i <morufataliyu@gmail.com>
2023-06-23 03:41:34 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line that printf function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a place of art,\n")
+	return (0);
+}
